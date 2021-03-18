e0d37c672b871e47481bbec4707c991687c56313
haruhiko-zht <psp.in.haruhiko@gmail.com>
2021-03-18 12:33:36 +0900

    [update]Apply React Router.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
+import { Navigate, Route, Routes, useLocation } from 'react-router';
+
 import Home from 'components/pages/Home';
 import './App.scss';
 
-const App: FC = () => (
-  <>
-    <div className="main-bg" />
-    <Home />
-  </>
-);
+const App: FC = () => {
+  const { hash, pathname } = useLocation();
+
+  useEffect(() => {
+    if (!hash) window.scrollTo(0, 0);
+  }, [hash, pathname]);
+
+  return (
+    <>
+      <div className="main-bg" />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Navigate to="/" replace />} />;
+      </Routes>
+    </>
+  );
+};
 
 export default App;
