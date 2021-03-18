89e95518a0850df723004c9ad000776d4ab2e8f0
haruhiko-zht <psp.in.haruhiko@gmail.com>
2021-03-18 15:21:43 +0900

    [add]Create Survey component.

---
+++ src/components/pages/Survey.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react';
+
+import Header from '../templates/Header';
+import Content from '../templates/Content';
+import Footer from '../templates/Footer';
+
+const Survey: FC = () => (
+  <>
+    <Header />
+    <Content title="調査備忘録" />
+    <Footer />
+  </>
+);
+
+export default Survey;
